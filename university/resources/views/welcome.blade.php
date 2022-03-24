9ce7d19971a29aede9346072261f7555d1a7b3bb
Saurabh Yadav <ysaurabh33@gmail.com>
2022-03-24 06:02:44 +0000

    initail commit

---
+++ university/resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link href="favicon.ico" rel="icon" />
+        <link rel="stylesheet" href="/css/app.css" />
+
+        <title>Uberflip Technical Challenge</title>
+    </head>
+    <body>
+        <div class="container mx-auto p-4">
+            <img class="w-16 h-16 mr-4 float-left" src="uberflip.png" alt="Logo" />
+            <h1>{{ $title ?? 'Welcome to the Uberflip Technical Challenge' }}</h1>
+        
+        </div>
+    </body>
+</html>
---
+++ university/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./resources/views/*.blade.php",
+    "./resources/js/*.js",
+    "./resources/js/*.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
